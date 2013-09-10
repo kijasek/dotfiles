3507dd72cea1f8fd8a97915f80b626b152c514c1
kijasek <kijasek@gmail.com>
2013-09-10 16:26:20 +0200

    Initial version of my dot files

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+nitrogen --restore
+~/.screenlayout/two_monitors.sh &
+(sleep 2s && tint2) &
+(sleep 3s && volti) &
+(sleep 4s && synapse) &
+(sleep 10s && parcellite) &
+(sleep 20s && conky -c /home/tkijas/.conky/Flat-Weather/conkyrc) &
