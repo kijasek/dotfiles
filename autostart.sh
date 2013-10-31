40fa8595bc1431742457956edc8be59792236b66
kijasek <kijasek@gmail.com>
2013-10-31 08:40:09 +0100

    eclipse solarized theme for chrome pluging; recent changes to config files

--- autostart.sh
+++ autostart.sh
@@ -3,5 +3,5 @@ nitrogen --restore
 (sleep 2s && tint2) &
 (sleep 3s && volti) &
 (sleep 4s && synapse) &
-(sleep 10s && parcellite) &
+(sleep 10s && glipper) &
 (sleep 20s && conky -c /home/tkijas/.conky/Flat-Weather/conkyrc) &
